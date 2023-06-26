9dcbef0244b1b25f81f77ef59e6f9d9d9f4a0044
Suhani Sinha <137385984+suhani-sinhaaa@users.noreply.github.com>
2023-06-26 22:58:54 +0530

    Add files via upload

---
+++ Assign2.py
@@ -0,0 +1,79 @@
+#arithmetic functions
+n1=int(input("Enter the first number "))
+n2=int(input("Enter the Second number "))
+print("Arithmetic Operators are:")
+print("Addition =",n1+n2)
+print("Subtraction =",n1-n2)
+print("Multiplication=",n1*n2)
+print("Division=",n1/n2)
+print("Floor Division =",n1//n2)
+print("Modulus=",n1%n2)
+print("Exponential =",n1**n2)
+
+#Assignment operators
+print("Assignment operators are:")
+a=6
+a+=1
+print("+=",a)
+a=8
+a-=2
+print("-=",a)
+a=2
+a*=2
+print("*=",a)
+a=8
+a/=2
+print("/=",a)
+a=2
+a**=4
+print("**=",a)
+a=8
+a//=2
+print("//=",a)
+a=8
+a%=4
+print("%=",a)
+
+#Comparison operators
+print("Comparison Operators:")
+print("'>' n1>n2=",n1>n2)
+print("'<' n1<n2=",n1<n2)
+print("'>=' n1>=n2 =",n1>=n2)
+print("'<=' n1<=n2 =",n1<=n2)
+print("'==' n1==n2=",n1==n2)
+print("'!=' n1!=n2=",n1!=n2)
+
+#logical operators
+a=True
+b=False
+print("and =",a and b)
+print("or =",a or b)
+print("not =",not a)
+
+#Bitwise operators
+print("Bitwise operators:")
+print("'|' n1|n2=",n1|n2)
+print("'&' n1&n2=",n1&n2)
+print("'^' n1^n2=",n1^n2)
+print("'<<' n1<<2=",n1<<2)
+print("'>>' n2>>2=",n2>>2)
+
+#Identity operator
+print("Identity operators:")
+a=15
+b=20
+print("is=",a is b)
+print("is not=",a is not b)
+
+#Membership operator
+print("Membership operators:")
+c="suhani"
+print("n"in c)
+print("z" not in c)
+
+
+
+
+
+
+
