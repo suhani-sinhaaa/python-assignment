97917f10fb884bf410983c85f31405bad287ac41
Suhani Sinha <137385984+suhani-sinhaaa@users.noreply.github.com>
2023-07-04 13:54:16 +0530

    Add files via upload

---
+++ Assign10.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import webbrowser
+
+def display():
+    s = e.get()
+    print("Navigating to", s)
+    webbrowser.open("https://www.instagram.com/" + s)
+
+obj = tk.Tk(className="interface")
+
+label = tk.Label(obj, text="Search User:",bg="grey")
+label.grid(row=3, column=2)
+
+e = tk.Entry(obj, font=("Times New Roman", 20), width=30,bg="light blue")
+e.grid(row=3, column=4)
+
+b = tk.Button(obj, text="Go", font=("Times New Roman", 12), command=display, bg="PINK")
+b.grid(row=4, column=3, columnspan=2)
+
+obj.mainloop()
