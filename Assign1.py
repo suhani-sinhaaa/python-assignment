4b6c37e330a859585d66c4936bcf859f451143c5
Suhani Sinha <137385984+suhani-sinhaaa@users.noreply.github.com>
2023-07-28 13:37:22 +0530

    Add files via upload

---
+++ Assign1.py
@@ -0,0 +1,5 @@
+n1=float(input("Enter the first number "))
+n2=float(input("Enter the second number "))
+n3=float(input("Enter the third number "))
+result=((n1+n2+n3)/3)
+print("The average of numbers" ,n1,",",n2,",",n3,"is" ,result)
